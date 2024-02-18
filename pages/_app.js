23ec44a91bfe05291c2220b9d87dc27560a0d6f8
이준호 <esens0607@gmail.com>
2024-02-18 11:03:48 +0900

    주석수정

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
-import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-export default function App({Component}) {
+export default function App({ Component }) {
   const client = new ApolloClient({
-    uri: 'http://practice.codebootcamp.co.kr/graphql',
+    uri: "http://practice.codebootcamp.co.kr/graphql", // 서버의 URI 주소를 작성
     cache: new InMemoryCache(), // PC 메모리에다가 백엔드에서 받은 데이터를 임시로 저장해 놓는다.
   });
   return (
