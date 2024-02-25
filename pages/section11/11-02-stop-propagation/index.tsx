f3accef280d9ea9e0fe09415c82dc25ba132c3c3
이준호 <esens0607@gmail.com>
2024-02-25 15:38:02 +0900

    20240225

--- pages/section11/11-02-stop-propagation/index.tsx
+++ pages/section11/11-02-stop-propagation/index.tsx
@@ -1,5 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
 import { MouseEvent } from "react";
+import Checkbox from "./checkbox";
 const FETCH_BOARDS = gql`
   query {
     fetchBoards {
@@ -11,7 +12,7 @@ const FETCH_BOARDS = gql`
   }
 `;
 
-export default function StaticRoutingBoardMovedPage() {
+export default function StopPropagationPage() {
   const { data } = useQuery(FETCH_BOARDS);
   console.log(data?.fetchBoards);
 
@@ -19,17 +20,22 @@ export default function StaticRoutingBoardMovedPage() {
     alert(event?.currentTarget.id + "님이 작성한 글입니다.");
   };
 
-  const qqq = () => {
-    alert("클릭 타이틀");
+  const qqq1 = () => {
+    alert("1번 클릭");
+  };
+
+  const qqq4 = (event) => {
+    event.stopPropagation();
+    alert("4번 클릭");
   };
   return (
     <div>
       {data?.fetchBoards.map((el: any) => (
-        <div id={el.writer} onClick={onClickAlert}>
-          <span>
-            <input type="checkbox" />
+        <div id={el.writer} onClick={qqq1}>
+          <Checkbox />
+          <span style={{ margin: "10px" }} onClick={qqq4}>
+            {el.number}
           </span>
-          <span style={{ margin: "10px" }}>{el.number}</span>
           <span style={{ margin: "10px" }}>{el.title}</span>
           <span style={{ margin: "10px" }}>{el.writer}</span>
         </div>
