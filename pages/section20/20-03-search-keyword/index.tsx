87edab3f2b952738ec7d596a299af6484cb32b5d
이준호 <esens0607@gmail.com>
2024-03-08 23:10:33 +0900

    20240308 search

---
+++ pages/section20/20-03-search-keyword/index.tsx
@@ -0,0 +1,78 @@
+import { gql, useQuery } from "@apollo/client";
+import type {
+  IQuery,
+  IQueryFetchBoardArgs,
+} from "../../../src/commons/types/generated/types";
+import type { ChangeEvent, MouseEvent } from "react";
+import { useState } from "react";
+import _ from "lodash";
+import { v4 as uuidv4 } from "uuid";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards($page: Int, $search: String) {
+    fetchBoards(page: $page, search: $search) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutingBoardMovedPage(): JSX.Element {
+  // const [search, setSearch] = useState("");
+
+  const [keyword, setKeyword] = useState("");
+  const { data, refetch } = useQuery<
+    Pick<IQuery, "fetchBoards">,
+    IQueryFetchBoardArgs
+  >(FETCH_BOARDS);
+
+  const onClickPage = (event: MouseEvent<HTMLSpanElement>): void => {
+    // 검색에서 refetch할 때, search 검색어가 refetch에 이미 저장되어 있는 상태로 search를 넣을 필요가 없다.
+    refetch({ page: Number(event.currentTarget.id) });
+  };
+
+  const getDebounce = _.debounce((value) => {
+    void refetch({
+      search: value,
+      page: 1,
+    });
+    setKeyword(value);
+  }, 500);
+
+  const onChangeSearch = (event: ChangeEvent<HTMLInputElement>): void => {
+    // setSearch(event.currentTarget.value);
+    getDebounce(event.currentTarget.value);
+  };
+
+  return (
+    <div>
+      검색어 입력 : <input type="text" onChange={onChangeSearch} />
+      {/* <button onClick={onClickSearch}>검색하기</button> */}
+      {data?.fetchBoards.map((el) => (
+        <div key={el._id}>
+          <span style={{ margin: "10px" }}>
+            {el.title
+              .replaceAll(keyword, `@#$${keyword}@#$`)
+              .split("@#$")
+              .map((el) => (
+                <span
+                  key={uuidv4()}
+                  style={{ color: el === keyword ? "red" : "black" }}
+                >
+                  {el}
+                </span>
+              ))}
+          </span>
+          <span style={{ margin: "10px" }}>{el.contents}</span>
+        </div>
+      ))}
+      {new Array(10).fill(1).map((_, index) => (
+        <span key={index + 1} id={String(index + 1)} onClick={onClickPage}>
+          <button>{index + 1}</button>
+        </span>
+      ))}
+    </div>
+  );
+}
